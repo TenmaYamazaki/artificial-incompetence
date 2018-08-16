5181385a1186274b8d4f47102d3c3549569db3c0
tenma.yamazaki <tenma.yamazaki@ari-jp.com>
2018-08-16 17:22:31 +0900

    通報修正

--- src/main/java/com/example/artificialincompetence/controllers/AiController.java
+++ src/main/java/com/example/artificialincompetence/controllers/AiController.java
@@ -42,8 +42,8 @@ public class AiController {
         if(adjective == 0 || noun == 0) {
             Reply reply = new Reply( "<p>なにが知りたいんだい？<p><br>" +
                     "<form id=\"addres\" action=\"/add/word\" method=\"POST\">\r\n" + 
-                        "<input type=\"hidden\" value=\"" + adjective + "\" id=\"adjective\" name=\"adjective\">" +
-                        "<input type=\"hidden\" value=\"" + noun + "\" id=\"noun\" name=\"noun\">" +
+                        "<input type=\"hidden\" value=\"" + adjective + "name=\"adjective\">" +
+                        "<input type=\"hidden\" value=\"" + noun + "name=\"noun\">" +
                         "<input id=\"addwordtext\" name=\"addwordtext\" type=\"text\" id=\"reply\" name=\"reply\" placeholder=\"質問の答えを入力してね\" />\r\n" + 
                         "<input id=\"addwordsubmit\" type=\"submit\" value=\"決定\" />\r\n" + 
                     "</form>");
@@ -57,12 +57,11 @@ public class AiController {
             } catch (NullPointerException e) {
                Reply reply = new Reply( "<p>ぼくにはわからないからおしえて!<p><br>" +
                                         "<form id=\"addres\" action=\"/add/res\" method=\"POST\">\r\n" + 
-                                            "<input type=\"hidden\" value=\"" + adjective + "\" id=\"adjective\" name=\"adjective\">" +
-                                            "<input type=\"hidden\" value=\"" + noun + "\" id=\"noun\" name=\"noun\">" +
+                                            "<input type=\"hidden\" value=\"" + adjective + "name=\"adjective\">" +
+                                            "<input type=\"hidden\" value=\"" + noun + "name=\"noun\">" +
                                             "<input id=\"addrestext\" name=\"addrestext\" type=\"text\" id=\"reply\" name=\"reply\" placeholder=\"質問の答えを入力してね\" />\r\n" + 
                                             "<input id=\"addressubmit\" type=\"submit\" value=\"決定\" />\r\n" + 
-                                        "</form>"
-                                        + "<p>※入力しない場合は一度ページを更新してください（鋭意修正中）");
+                                        "</form>");
                return reply;
             }
         }
